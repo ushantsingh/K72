c0d2fe79fd8476d96bb700bf3d82362fff89ccc1
Ushant Singh <ushantsingh4@gmail.com>
2026-05-01 22:08:02 +0530

    k72

--- src/components/home/Videos.jsx
+++ src/components/home/Videos.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Videos = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted  src="../../../public/front_video.mp4"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted playsInline src="/front_video.mp4"></video>
     </div>
   )
 }
